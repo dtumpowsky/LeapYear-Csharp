61f494e655560389259b410d3834b5e17109aa01
CJ Rhone and David Tumpowsky <cj@>
2018-04-23 11:25:49 -0700

    Clarify notes

--- LeapYear/Models/LeapYear.cs
+++ LeapYear/Models/LeapYear.cs
@@ -1,10 +1,21 @@
 namespace LeapYear
 {
-  public class LeapYear
-  {
-    public bool IsLeapYear(int year)
+    public class LeapYear
     {
-      return year % 4 == 1;
+        public bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return year % 4 == 0;
+            }
+        }
     }
-  }
 }
