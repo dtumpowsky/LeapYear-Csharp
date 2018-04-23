b3514b351d28f52c164ff380f5526f29e7bbb96e
CJ Rhone and David Tumpowsky <cj+david@>
2018-04-23 09:57:16 -0700

    Add parameters for leap year test

--- LeapYear.Tests/ModelTests/LeapYearTest.cs
+++ LeapYear.Tests/ModelTests/LeapYearTest.cs
@@ -3,15 +3,16 @@ using LeapYear;
 
 namespace LeapYear.Tests
 {
-  [TestClass]
-  public class LeapYearTest
-  {
-    [TestMethod]
-    public void IsLeapYear_NumberDivisibleByFour_True()
+    [TestClass]
+    public class LeapYearTest
     {
-      LeapYear testLeapYear = new LeapYear();
-      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
-      //2012 paramater because divisible by 4 and returns true
+        [TestMethod]
+        public void IsLeapYear_NumberDivisibleByFour_True()
+        {
+              LeapYear testLeapYear = new LeapYear();
+              Assert.AreEqual(true, testLeapYear.IsLeapYear(2016));
+              //checks if number in parameter follows IsLeapYear method from Model
+              // The test passes through its own parameter "IsLeapYear(number)"
+        }
     }
-  }
 }
